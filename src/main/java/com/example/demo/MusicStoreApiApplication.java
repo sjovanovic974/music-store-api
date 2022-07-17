10d1c9f4c37e8538acbf75bee73c689d7c753e65
sjovanovic974 <sjovanovic974@gmail.com>
2022-07-17 13:32:56 +0200

    Initial commit. Bootstraping App. Closes #1

---
+++ src/main/java/com/example/demo/MusicStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApiApplication.class, args);
+	}
+
+}
