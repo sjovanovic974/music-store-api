3aa27592cbd826155a540f3b044c3a93df3540e4
sjovanovic974 <sjovanovic974@gmail.com>
2022-10-24 14:42:27 +0200

    Role and authority protection of routes added

--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -5,12 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static com.example.demo.security.registration.model.UserPermission.PRODUCT_WRITE;
+import static com.example.demo.security.registration.model.UserRole.ADMIN;
+import static com.example.demo.security.registration.model.UserRole.USER;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -29,19 +33,26 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        AuthenticationManagerBuilder authenticationManagerBuilder =
+        /*AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
 
-        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);*/
 
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .antMatchers(WHITE_LIST_URLS).permitAll()
+                        .antMatchers(DELETE, "/api/products/**").hasAuthority(PRODUCT_WRITE.getPermission())
+                        .antMatchers(PUT, "/api/products/**").hasAuthority(PRODUCT_WRITE.getPermission())
+                        .antMatchers(POST, "/api/products/**").hasAuthority(PRODUCT_WRITE.getPermission())
+                        .antMatchers("/api/artists/**").hasRole(ADMIN.name())
+                        .antMatchers("/api/product-categories/**").hasRole(ADMIN.name())
+                        .antMatchers("/api/products/**").hasAnyRole(USER.name(), ADMIN.name())
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider)
                 .build();
     }
 }
